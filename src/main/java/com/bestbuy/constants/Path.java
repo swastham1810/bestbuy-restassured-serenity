09751250f80a0f9d063d88be53a33c986b66d757
prishn <kakadiyaasha@googlemail.com>
2024-01-22 14:09:19 +0000

    Bestbuy RestAssured Serenity

---
+++ src/main/java/com/bestbuy/constants/Path.java
@@ -0,0 +1,24 @@
+package com.bestbuy.constants;
+
+/**
+ * Created by Jay
+ */
+public class Path {
+
+    /*
+     * This is base path for products api
+     */
+    public static final String PRODUCTS = "/products";
+    /*
+     * This is base path for stores api
+     */
+    public static final String STORES = "/stores";
+
+    /*
+     * This is base path for authenticate api
+     */
+    /*public static final String AUTHENTICATE = "/authenticate";
+
+    public static final String ACCOUNT = "/account";*/
+
+}
